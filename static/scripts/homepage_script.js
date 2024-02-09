b3f882803bcfea8853b5f039dd0a585e838dc7e7
KapitaenKirSche <benjamin@marx-merten.de>
2024-02-09 10:18:16 +0100

    working on ui

---
+++ static/scripts/homepage_script.js
@@ -0,0 +1,14 @@
+function openRecipePage() {
+    // Hier kannst du die Funktion für das Öffnen der Rezeptseite implementieren
+    console.log("Öffne Rezeptseite");
+}
+
+function openInventoryPage() {
+    // Hier kannst du die Funktion für das Öffnen der Vorratsschrankseite implementieren
+    console.log("Öffne Vorratsschrankseite");
+}
+
+function openShoppingListPage() {
+    // Hier kannst du die Funktion für das Öffnen der Einkaufslistenseite implementieren
+    console.log("Öffne Einkaufslistenseite");
+}
